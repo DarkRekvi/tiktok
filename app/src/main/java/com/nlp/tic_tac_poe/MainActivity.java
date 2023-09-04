ddb5368e187bec29d8df34a896afff576765b53a
lubuntum <lubuntum@gmail.com>
2023-09-04 14:17:24 +0700

    Добавили логику ходов для двух игроков

--- app/src/main/java/com/nlp/tic_tac_poe/MainActivity.java
+++ app/src/main/java/com/nlp/tic_tac_poe/MainActivity.java
@@ -7,6 +7,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.nlp.tic_tac_poe.databinding.GameBoardBinding;
+
+import java.util.ArrayList;
+
 /*
 * Этапы сохранения проекта в git
 * git init - инициализация гита в проекта
@@ -14,24 +19,46 @@ import android.widget.Toast;
 * git commit -m "текст сообщения"
 * */
 public class MainActivity extends AppCompatActivity {
+    public GameBoardBinding binding;
+    public ArrayList<Button> squaresArray;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //Вызываем родительский метод тот же
         super.onCreate(savedInstanceState);
+        binding = GameBoardBinding.inflate(getLayoutInflater());
         //Ссылаемся на интерфейс
-        setContentView(R.layout.activity_main);
-        //Задаем ссылки на элементы интерфейса
-        Button showContentButton = findViewById(R.id.content_button);
-        EditText contentInput = findViewById(R.id.content_input);
-        showContentButton.setOnClickListener((view) ->{
-            String contentStr = contentInput.getText().toString();
-            //Если в поле contentInput
-            // нету текста, то выходим из метода
-            if(contentStr.equals("")) return;
-            Toast toast = Toast.makeText(this, contentStr, Toast.LENGTH_LONG);
-            toast.show();
-        });
+        setContentView(binding.getRoot());
+        boardInit();
 
+    }
+    public void boardInit(){
+
+        squaresArray = new ArrayList<>();
+        squaresArray.add(binding.field1);
+        squaresArray.add(binding.field2);
+        squaresArray.add(binding.field3);
+        squaresArray.add(binding.field4);
+        squaresArray.add(binding.field5);
+        squaresArray.add(binding.field6);
+        squaresArray.add(binding.field7);
+        squaresArray.add(binding.field8);
+        squaresArray.add(binding.field9);
+
+        for(int i = 0; i < squaresArray.size();i++){
+            int finalI = i;
+            squaresArray.get(i).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(!squaresArray.get(finalI).getText().toString().equals(" "))
+                        return;
+                    if(GameInfo.whoIsNext)
+                        squaresArray.get(finalI).setText(GameInfo.firstPlayerSymbol);
+                    else
+                        squaresArray.get(finalI).setText(GameInfo.secondPlayerSymbol);
+                    GameInfo.whoIsNext = !GameInfo.whoIsNext;
+                }
+            });
+        }
     }
 
 }
